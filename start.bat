472ed7c26de626ba5bfb901a804b0ee926e0c943
tan-o <44334636+tan-o@users.noreply.github.com>
2026-02-05 22:39:18 +0800

    Replace hardcoded paths with placeholders in start.bat
    Updated OpenCV and LLVM paths to placeholders for user customization.

--- start.bat
+++ start.bat
@@ -1,8 +1,8 @@
 @echo off
 :: 只在当前窗口临时增加 OpenCV 和 LLVM 路径
-set PATH=C:\Users\Feysen\scoop\apps\opencv\current\x64\vc16\bin;C:\Users\Feysen\scoop\apps\llvm\current\bin;%PATH%
+set PATH=<your opencv path>\x64\vc16\bin;<your llvm path>\bin;%PATH%
 :: 强制指定 OpenCV 配置路径解决之前的 CMake 报错
-set OpenCV_DIR=C:\Users\Feysen\scoop\apps\opencv\current\x64\vc16\lib
+set OpenCV_DIR=<your opencv path>\x64\vc16\lib
 set OPENCV_LINK_LIBS=opencv_world4120
 
 cargo run --release
